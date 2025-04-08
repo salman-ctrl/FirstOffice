5530d633ea6fe6ce6245ee381fa4a0c7144ce73d
salman-ctrl <zirmanvictory@gmail.com>
2025-04-09 02:11:37 +0700

    add relasi model

--- firstofficeBe/app/Models/BookingTransaction.php
+++ firstofficeBe/app/Models/BookingTransaction.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class BookingTransaction extends Model
@@ -21,4 +22,19 @@ class BookingTransaction extends Model
         'ended_at',
         'office_space_id',
     ];
+
+    public static function generateUniqueTrxId()
+    {
+        $prefix = 'FO';
+        do {
+            $randomString = $prefix . mt_rand(1000, 9999);
+        } while (self::where('booking_trx_id', $randomString)->exists());
+
+        return $randomString;
+    }
+
+    public function officeSpace(): BelongsTo
+    {
+        return $this->belongsTo(OfficeSpace::class, 'office_space_id');
+    }
 }
--- firstofficeBe/app/Models/OfficeSpace.php
+++ firstofficeBe/app/Models/OfficeSpace.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class OfficeSpace extends Model
@@ -23,7 +25,20 @@ class OfficeSpace extends Model
         'city_id',
     ];
 
+    public function photos(): HasMany
+    {
+        return $this->hasMany(OfficeSpacePhoto::class);
+    }
 
+    public function benefits(): HasMany
+    {
+        return $this->hasMany(OfficeSpaceBenefit::class);
+    }
+
+    public function city(): BelongsTo
+    {
+        return $this->belongsTo(City::class);
+    }
 
 
 }
