a83191c00a2329e892395b724bdfa5ded57ed4f0
salman-ctrl <zirmanvictory@gmail.com>
2025-04-09 02:03:19 +0700

    add relasi model

--- firstofficeBe/app/Models/City.php
+++ firstofficeBe/app/Models/City.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class City extends Model
@@ -15,4 +16,10 @@ class City extends Model
         'slug',
         'photo',
     ];
+
+    public function officeSpaces(): HasMany
+    {
+        return $this->hasMany(OfficeSpace::class);
+    }
+
 }
