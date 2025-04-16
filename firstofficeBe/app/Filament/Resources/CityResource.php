964af04513f0f94528d0d1349e90fc499a0d0b18
salman-ctrl <zirmanvictory@gmail.com>
2025-04-16 22:49:05 +0700

    masih berjunag clear error

--- firstofficeBe/app/Filament/Resources/CityResource.php
+++ firstofficeBe/app/Filament/Resources/CityResource.php
@@ -60,10 +60,7 @@ class CityResource extends Resource
 
     public static function getRelations(): array
     {
-        return [
-            //
-        ];
-    }
+        
 
     public static function getPages(): array
     {
