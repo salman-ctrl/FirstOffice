900ade37b07fc02ba52141205308b4c6ee964f96
salman-ctrl <zirmanvictory@gmail.com>
2025-04-19 23:14:10 +0700

    tambah data booking transaction

--- firstofficeBe/app/Filament/Resources/BookingTransactionResource.php
+++ firstofficeBe/app/Filament/Resources/BookingTransactionResource.php
@@ -24,6 +24,47 @@ class BookingTransactionResource extends Resource
         return $form
             ->schema([
                 //
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+
+                Forms\Components\TextInput::make('booking_trx_id')
+                    ->required()
+                    ->maxLength(255),
+
+                Forms\Components\TextInput::make('phone_number')
+                    ->required()
+                    ->maxLength(255),
+
+                Forms\Components\TextInput::make('total_amount')
+                    ->required()
+                    ->numeric()
+                    ->prefix('IDR'),
+
+                Forms\Components\TextInput::make('duration')
+                    ->required()
+                    ->numeric()
+                    ->prefix('Days'),
+
+                Forms\Components\DatePicker::make('started_at')
+                    ->required(),
+
+                Forms\Components\DatePicker::make('ended_at')
+                    ->required(),
+
+                Forms\Components\Select::make('is_paid')
+                    ->options([
+                        true => 'Paid',
+                        false => 'Not Paid',
+                    ])
+                    ->required(),
+
+                Forms\Components\Select::make('office_space_id')
+                    ->relationship('officeSpace', 'name')
+                    ->searchable()
+                    ->preload()
+                    ->required(),
+
             ]);
     }
 
